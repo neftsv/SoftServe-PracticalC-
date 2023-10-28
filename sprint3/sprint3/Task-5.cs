0444b2af6dc56b468a88aa67d12fe2679b4191f1
neftsv <svkush2013@gmail.com>
2023-10-28 22:33:05 +0300

    Add 5th task. Complete Sprint3

---
+++ sprint3/sprint3/Task-5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public delegate void EventHandler();
+
+class EventProgram{
+    public event EventHandler Show;
+
+    public EventProgram()
+    {
+        Show += Dog;
+        Show += Cat;
+        Show += Mouse;
+        Show += Elephant;
+
+        // Invoke the Show event
+        Show.Invoke();
+    }
+
+
+
+    public static void Dog()
+    {
+        Console.WriteLine("Dog");
+    }
+
+    public static void Cat()
+    {
+        Console.WriteLine("Cat");
+    }
+
+    public static void Mouse()
+    {
+        Console.WriteLine("Mouse");
+    }
+
+    public static void Elephant()
+    {
+        Console.WriteLine("Elephant");
+    }
+
+}
