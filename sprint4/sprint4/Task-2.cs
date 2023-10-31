df6e0e2cbe3a0100cd6c7f4dca6907d58f8e3c62
neftsv <svkush2013@gmail.com>
2023-11-01 01:15:09 +0200

    Add 2nd task

---
+++ sprint4/sprint4/Task-2.cs
@@ -0,0 +1,19 @@
+﻿interface IAnimal
+{
+    string Name { get { return ""; } }
+    void Voice();
+    void Feed();
+}
+
+class Dog : IAnimal
+{
+    public void Voice() { Console.WriteLine("Woof"); }
+    public void Feed() { Console.WriteLine("I eat meat"); }
+
+}
+
+class Cat : IAnimal
+{
+    public void Voice() { Console.WriteLine("Mew"); }
+    public void Feed() { Console.WriteLine("I eat mice"); }
+}
