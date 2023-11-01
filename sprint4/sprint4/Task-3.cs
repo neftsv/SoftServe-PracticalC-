3e680add84aab08e6abf0948e7a98ecdd285ea71
neftsv <svkush2013@gmail.com>
2023-11-01 23:18:19 +0200

    Add 4th sprint

---
+++ sprint4/sprint4/Task-3.cs
@@ -0,0 +1,56 @@
+﻿public enum ColourEnum
+{
+    Red,
+    Green,
+    Blue
+}
+
+interface IColoured
+{
+    ColourEnum Colour { get { return ColourEnum.Red; } }
+}
+
+interface IDocument
+{
+    static string defaultText = "Lorem ipsum";
+    public int Pages { get { return 0; } set { Pages = 0; } }
+    public string Name { get; set; }
+    public void AddPages(int numberOfPages)
+    {
+        Pages += numberOfPages;
+    }
+
+    public void Rename(string newName)
+    {
+        Name = newName;
+    }
+}
+
+class ColouredDocument : IDocument, IColoured
+{
+    public int Pages { get; set; }
+    public string Name { get; set; }
+    public ColourEnum Colour { get; set; }
+
+    public ColouredDocument()
+    {
+
+    }
+
+    public ColouredDocument(ColourEnum colour)
+    {
+
+    }
+}
+
+class Example
+{
+    public static void Do()
+    {
+        ColouredDocument doc1 = new ColouredDocument();
+        doc1.Name = "Document1";
+        Console.WriteLine(doc1.Name);
+        ((IDocument)doc1).Rename("Document2");
+        Console.WriteLine(doc1.Name);
+    }
+}
---
+++ sprint4/sprint4/Task-4.cs
@@ -0,0 +1,85 @@
+﻿interface IShape
+{
+    double Area() => 0;
+}
+
+interface ICloneable
+{
+    object Clone();
+}
+
+interface IComparable<T>
+{
+    int CompareTo(T other);
+}
+
+interface IComparer<T>
+{
+    int Compare(T obj1, T obj2);
+}
+
+class Rectangle : IShape
+{
+    double Length { get; set; }
+    double Width { get; set; }
+
+    double Area() => Length * Width;
+}
+
+class Trapezoid : IShape
+{
+    double Length1 { get; set; }
+    double Length2 { get; set; }
+    double Width { get; set; }
+    double Area () => 0.5 * (Length1 + Length2) * Width;
+    
+}
+
+class Room<T> where T : IShape, ICloneable, IComparable
+{
+    double Heigth { get; set; }
+    T Floor{ get; set;}
+
+    public double Volume() => Floor.Area() * Heigth;
+
+    public object Clone()
+    {
+        return new Room<T>
+        {
+            Heigth = this.Heigth,
+            Floor = this.Floor
+        };
+    }
+
+    public int CompareTo(object obj)
+    {
+        Room<T> room = obj as Room<T>;
+        if (obj != null) return this.Floor.Area().CompareTo(room.Floor.Area());
+        else throw new Exception("Error");
+    }
+}
+
+class RoomComparerByVolume<T> : IComparer<Room<T>> where T : IShape
+{
+    public int Compare(Room<T> x, Room<T> y)
+    {
+        if (x == null || y == null)
+        {
+            throw new ArgumentNullException("One or both of the rooms to compare are null.");
+        }
+
+        return x.Volume().CompareTo(y.Volume());
+    }
+}
+//class RoomComparerByVolume<T> : IComparer<Room> where T : IShape
+//{
+//    public int Compare(T room1, T room2)
+//    {
+//        if (room1 == null || room2 == null)
+//        {
+//            throw new ArgumentNullException("One or both of the rooms to compare are null.");
+//        }
+
+//        if(room1.)
+//    }
+//}
