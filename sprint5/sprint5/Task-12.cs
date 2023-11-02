994fb3dd52db50449510fde2cc667ec859b54903
neftsv <svkush2013@gmail.com>
2023-11-02 22:14:21 +0200

    Add 2nd and 3rd tasks of lvl1

---
+++ sprint5/sprint5/Task-12.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+class Task2
+{
+    public static void SearchKeys(Dictionary<string, string> persons)
+    {
+        foreach (string key in persons.Keys)
+        {
+            Console.WriteLine(key);
+        }
+    }
+
+    public static void SearchValues(Dictionary<string, string> person)
+    {
+        foreach (string value in person.Values)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public static void SearchAdmin(Dictionary<string, string> persons)
+    {
+        foreach (var pers in persons)
+        {
+            if(pers.Value == "Admin") 
+            {
+                Console.WriteLine(pers.Key + " " + pers.Value);
+                break;
+            }
+        }
+    }
+}
---
+++ sprint5/sprint5/Task-13.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class Product
+{
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+
+    public static void TotalPrice(ILookup<string, Product> lookup)
+    {
+        foreach (var category in lookup)
+        {
+            decimal totalPrice = 0;
+            foreach (var item in category)
+            {
+                totalPrice += item.Price;
+                Console.WriteLine(item.Name + " " + item.Price);
+            }
+            Console.WriteLine(category.Key + " " + totalPrice);
+        }
+    }
+}
