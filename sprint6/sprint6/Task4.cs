ba6747f505d5c1a0f42c79c14919e5c97b1a6556
neftsv <svkush2013@gmail.com>
2023-11-08 03:10:50 +0200

    Add 4th task

---
+++ sprint6/sprint6/Task4.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+class ShowPowerRange
+{
+    public static IEnumerable<int> PowerRanger(int degree, int start, int finish)
+    {
+        if (start > finish || start < 0 || finish < 0 || degree < 0)
+        {
+            yield return 0;
+        }
+        else if (start < finish)
+        {
+            if (degree == 0) { yield return 1; yield break; }
+            int result = 1;
+            for (int i = 1; result < finish; i++)
+            {
+                result = i;
+                for (int j = 1; j < degree; j++)
+                {
+                    result *= i;
+                }
+                if (result <= finish && result >= start)
+                    yield return result;
+            }
+        }
+    }
+}
