e88f07b1d23013e54752a94919fbd9d66bb316dd
neftsv <svkush2013@gmail.com>
2023-11-10 22:38:53 +0200

    Add 2st task

---
+++ sprint7/sprint7/Task-2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint7
+{
+    internal class Task_2
+    {
+        public static double EvaluateAggregate(double[] inputData, Func<double, double, double> aggregate, Func<double, int, bool> predicate)
+        {
+            double res = 0;
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                if (predicate(inputData[i], i))
+                    res = aggregate(res, inputData[i]);
+            }
+            return res;
+        }
+    }
+}
