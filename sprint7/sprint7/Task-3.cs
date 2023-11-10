878102c9f899e4d18f085cef5ef4ac8492a87056
neftsv <svkush2013@gmail.com>
2023-11-10 23:38:51 +0200

    Add 3rd task

---
+++ sprint7/sprint7/Task-3.cs
@@ -0,0 +1,22 @@
+﻿internal class Task_3
+{
+    public static int ProductWithCondition(List<int> integers, Func<int, bool> predicate)
+    {
+        List<int> newIntegers = new List<int>();
+        for (int i = 0; i < integers.Count; i++)
+        {
+            if (predicate(integers[i]))
+                newIntegers.Add(integers[i]);
+        }
+
+        if (newIntegers.Count == 0) return 1;
+
+        int res = 1;
+        for (int i = 0; i < newIntegers.Count; i++)
+        {
+            res *= newIntegers[i];
+        }
+        return res;
+    }
+}
+
