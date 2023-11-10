5998e02c5974871fce7c5a31a6eba40f2b7c10ee
neftsv <svkush2013@gmail.com>
2023-11-10 23:47:18 +0200

    Modified 3rd task

--- sprint7/sprint7/Task-3.cs
+++ sprint7/sprint7/Task-3.cs
@@ -2,21 +2,14 @@
 {
     public static int ProductWithCondition(List<int> integers, Func<int, bool> predicate)
     {
-        List<int> newIntegers = new List<int>();
+        if (integers.Count == 0) return 1;
+
+        int res = 1;
         for (int i = 0; i < integers.Count; i++)
         {
             if (predicate(integers[i]))
-                newIntegers.Add(integers[i]);
-        }
-
-        if (newIntegers.Count == 0) return 1;
-
-        int res = 1;
-        for (int i = 0; i < newIntegers.Count; i++)
-        {
-            res *= newIntegers[i];
+                res *= integers[i];
         }
         return res;
     }
 }
-
