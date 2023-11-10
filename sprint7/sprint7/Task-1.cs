79f56c3bc88b944f5d206d54420e8b809edc8189
neftsv <svkush2013@gmail.com>
2023-11-10 22:22:52 +0200

    Add 1st task

---
+++ sprint7/sprint7/Task-1.cs
@@ -0,0 +1,12 @@
+﻿class P
+{
+    public static double EvaluateSumOfElementsOddPositions(double[] inputData)
+    {
+        double sum = 0;
+        for (int i = 1; i < inputData.Length; i += 2)
+        {
+            sum += inputData[i];
+        }
+        return sum;
+    }
+}
