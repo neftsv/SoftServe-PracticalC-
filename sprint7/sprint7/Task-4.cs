9c47bd24649707d63ad6403b5a38ae0baba05b62
neftsv <svkush2013@gmail.com>
2023-11-11 22:05:49 +0200

    Add 4th task

--- sprint7/sprint7/Task-4.cs
+++ sprint7/sprint7/Task-4.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sprint7
+﻿using System.Collections.Generic;
+internal class Task_4
 {
-    internal class Task_4
+    public static string GetWord(string input, string seed)
     {
+        string[] words = input.Split(' ');
+        
+        string longestWord = seed;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > longestWord.Length)
+                longestWord = words[i];
+        }
+        if (longestWord == "") return seed;
+
+        string res = "";
+        for (int i = 0; i < longestWord.Length; i++)
+        {
+            if (longestWord[i] == 'a' || res != "")
+            {
+                res += longestWord[i];
+            }
+        }
+        return res;
     }
 }
---
+++ sprint7/sprint7/Task-5.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+class Worker
+{
+    public int Id {  get; set; }
+    public string Name { get; set; }
+    public decimal Salary {  get; set; }
+    public Department Department { get; set; }
+    public Worker(string name, decimal salary, Department department)
+    {
+        Name = name;
+        Salary = salary;
+        Department = department;
+    }
+
+    public string Serialize()
+    {
+        var workerJson = new
+        {
+            Full_name = Name,
+            Salary,
+            Department = new
+            {
+                Name = Department.Name,
+                Id = Department.Id,
+                Manager = Department.Manager != null ? new
+                {
+                    Full_name = Department.Manager.Name,
+                    Salary = Department.Manager.Salary
+                } : null
+            }
+        };
+
+        return JsonSerializer.Serialize(workerJson);
+    }
+
+    public static Worker Deserialize(string json)
+    {
+        var workerJson = JsonSerializer.Deserialize<WorkerJson>(json);
+
+        return CreateWorkerFromWorkerJson(workerJson);
+    }
+    private class WorkerJson
+    {
+        public string Full_name { get; set; }
+        public decimal Salary { get; set; }
+        public DepartmentJson Department { get; set; }
+    }
+
+    private class DepartmentJson
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public WorkerJson Manager { get; set; }
+    }
+    private static Worker CreateWorkerFromWorkerJson(WorkerJson workerJson)
+    {
+        var manager = workerJson.Department?.Manager != null
+            ? new Worker(workerJson.Department.Manager.Full_name, workerJson.Department.Manager.Salary, null)
+            : null;
+
+        return new Worker(workerJson.Full_name, workerJson.Salary, new Department(workerJson.Department.Name, workerJson.Department.Id, manager));
+    }
+}
+class Department
+{
+    public string Name { get; set; }
+    public int Id {  get; set; }
+    public Worker Manager { get; set; }
+
+    public Department(string name, int id, Worker manager)
+    {
+        Name = name;
+        Id = id;
+        Manager = manager;
+    }
+    
+}
+
+//class Pr
+//{
+//    static void Main()
+//    {
+//        Worker w = new Worker("Anna", 700, new Department("Mechanics", 1, new Worker("Tom", 600, null)));
+
+//        // Serialize
+//        string serializedWorker = w.Serialize();
+//        Console.WriteLine(serializedWorker);
+
+//        // Deserialize
+//        Worker deserializedWorker = Worker.Deserialize(serializedWorker);
+//        //Console.WriteLine($"Deserialized Worker: {deserializedWorker.Name}, Salary: {deserializedWorker.Salary}");
+//    }
+//}
+
