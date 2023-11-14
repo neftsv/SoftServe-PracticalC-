239cb80cd058209d4870d92ae4ad19e39dffe998
neftsv <svkush2013@gmail.com>
2023-11-14 23:39:56 +0200

    Add 1-st task

---
+++ sprint8/sprint8/Task-1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Intrinsics;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class ParallelUtils<T, TR>
+{
+    private Func<T, T, TR> operation;
+    private T operand1;
+    private T operand2;
+    public TR Result { get; set; }
+    public ParallelUtils(Func<T, T, TR> operation, T operand1, T operand2)
+    {
+        this.operation = operation;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    public void StartEvaluation()
+    {
+        Thread thread = new(() => Result = operation(operand1, operand2));
+        thread.Start();
+    }
+
+    public void Evaluate()
+    {
+        Thread thread = new(() => Result = operation(operand1, operand2));
+        thread.Start();
+        thread.Join();
+    }
+}
