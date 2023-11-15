b33056313a3fdfded91540006977eb1653c522ee
neftsv <svkush2013@gmail.com>
2023-11-15 11:09:25 +0200

    Replace 5-th task

--- sprint8/sprint8/Task-5.cs
+++ sprint8/sprint8/Task-5.cs
@@ -16,18 +16,17 @@ class MyThreads
     {
         Thread1 = new Thread(() =>
         {
-            lock (Den)
+            lock (Ada)
             {
                 for (int i = 0; i < 5; i++, n++)
                 {
                     Console.WriteLine("Thread1 n = " + n);
                     
                 }
-
-                Monitor.Exit(Den);
-
-                lock (Ada)
+                Thread.Yield();
+                lock (Den)
                 {
+                    
                     for (int i = 0; i < 5; i++, m++)
                         Console.WriteLine("Thread1 m = " + m);
                     Console.WriteLine("Thread1 success!");
@@ -37,17 +36,17 @@ class MyThreads
 
         Thread2 = new Thread(() =>
         {
-            lock (Ada)
+            lock (Den)
             {
+               
                 for (int i = 0; i < 5; i++, m++)
                     Console.WriteLine("Thread2 m = " + m);
-                
-                lock (Den)
-                {
+
+
                     for (int i = 0; i < 5; i++, n++)
                         Console.WriteLine("Thread2 n = " + n);
                     Console.WriteLine("Thread2 success!");
-                }
+
             }
         });
     }
