34f1f8aad23206b9159926241d91c2af46bcf71e
neftsv <svkush2013@gmail.com>
2023-11-21 14:43:07 +0200

    Add 1st task

---
+++ sprint10/sprint10/Task-1.cs
@@ -0,0 +1,30 @@
+﻿namespace Sprint10_Task1
+{
+    class Customer
+    {
+        public void Register(string email, string password)
+        {
+            try
+            {
+                MailService mailService = new MailService();
+                mailService.SendEmail(email, "Email  title", "Email body");
+
+            }
+            catch { throw; }
+        }
+    }
+
+    class MailService
+    {
+        public bool ValidEmail(string email)
+        {
+            return email.Contains("@");
+        }
+
+        public void SendEmail(string email, string emailTitle, string emailBody)
+        {
+            if (ValidEmail(email))
+                Console.WriteLine(string.Format("Mail:{0}, Title:{1}, Body:{2}", email, emailTitle, emailBody));
+        }
+    }
+}
